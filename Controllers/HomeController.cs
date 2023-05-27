68e819084764e178f386274b43be3702ed8cb7a3
M-Laswad <binlaswadmohammed@gmail.com>
2023-05-27 05:43:28 +0300

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web3_hw3.Models;
+
+namespace Web3_hw3.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Blog()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Services()
+    {
+        return View();
+    }
+    public IActionResult Single()
+    {
+        return View();
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web3_hw3.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
